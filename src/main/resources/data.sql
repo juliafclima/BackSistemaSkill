3225b0f2fef2ad4efabbd139b2280ee19b219368
Júlia Lima <juliafclima@hotmail.com>
2024-04-20 00:05:35 -0300

    consultas sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,21 +1,9 @@
-INSERT INTO public.usuario(id, login, senha, situacao) VALUES 
-	(1, 'julia', '123', 'PENDENTE'),
-	(2, 'mateus', '123', 'ATIVO'),
-	(3, 'liz', '123', 'INATIVO');
-	
-INSERT INTO public.skill(id, nome, descricao, url) VALUES 
-	(1, 'Python', 'Linguagem de programação interpretada', 'https://www.python.org/'),
-	(2, 'JavaScript', 'Linguagem de programação de alto nível', 'https://developer.mozilla.org/pt-BR/docs/Web/JavaScript'),
-	(3, 'Java', 'Linguagem de programação de propósito geral', 'https://www.java.com/pt-BR/'),
-	(4, 'HTML', 'Linguagem de marcação utilizada na criação de páginas web', 'https://developer.mozilla.org/pt-BR/docs/Web/HTML'),
-	(5, 'CSS', 'Linguagem de estilo utilizada para estilizar elementos HTML', 'https://developer.mozilla.org/pt-BR/docs/Web/CSS'),
-	(6, 'SQL', 'Linguagem de consulta estruturada para manipular bancos de dados', 'https://www.w3schools.com/sql/');
 
-INSERT INTO public.usuario_skill (id, id_usuario, id_skill, level) VALUES 
-	(1, 1, 1, '9/10'),
-	(2, 2, 2, '2/10'),
-	(3, 3, 3, '5/10'),
-	(4, 1, 4, '1/10'),
-	(5, 2, 5, '7/10'),
-	(6, 3, 6, '3/10');
-	
+-- Inserir na tabela skill
+INSERT INTO public.skill(id, nome, descricao, url) VALUES 
+    (1, 'Python', 'Linguagem de programação interpretada', 'https://th.bing.com/th/id/OIP.dJToM1TiZiJA0GYwzDHwjQHaHY?rs=1&pid=ImgDetMain'),
+    (2, 'JavaScript', 'Linguagem de programação de alto nível', 'https://th.bing.com/th/id/R.f6302414f1a97b960efbc44cd0b7bf5e?rik=TQ%2bfMsswbcgl3w&pid=ImgRaw&r=0'),
+    (3, 'Java', 'Linguagem de programação de propósito geral', 'https://th.bing.com/th/id/OIP.n8pa_ux7uUyU9CJrzb1scAHaHa?rs=1&pid=ImgDetMain'),
+    (4, 'HTML', 'Linguagem de marcação utilizada na criação de páginas web', 'https://th.bing.com/th/id/OIP.lxMbLFRw0sgOlOwjC62ySwHaKc?rs=1&pid=ImgDetMain'),
+    (5, 'CSS', 'Linguagem de estilo utilizada para estilizar elementos HTML', 'https://th.bing.com/th/id/R.7c12764796349bfb505d4c5d73489c3d?rik=bM4AFS0UAqY17g&pid=ImgRaw&r=0'),
+    (6, 'SQL', 'Linguagem de consulta estruturada para manipular bancos de dados', 'https://th.bing.com/th/id/OIP.7P45uhOM8sYH2dZVY_9XpQHaFk?rs=1&pid=ImgDetMain');
