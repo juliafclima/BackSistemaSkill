0a90637cff5e4d3145d6ba87d1cb97a7f783933f
Júlia Lima <juliafclima@hotmail.com>
2024-04-20 22:17:06 -0300

    mais dados na consulta de skill

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,4 +6,8 @@ INSERT INTO public.skill(id, nome, descricao, url) VALUES
     (3, 'Java', 'Linguagem de programação de propósito geral', 'https://th.bing.com/th/id/OIP.n8pa_ux7uUyU9CJrzb1scAHaHa?rs=1&pid=ImgDetMain'),
     (4, 'HTML', 'Linguagem de marcação utilizada na criação de páginas web', 'https://th.bing.com/th/id/OIP.lxMbLFRw0sgOlOwjC62ySwHaKc?rs=1&pid=ImgDetMain'),
     (5, 'CSS', 'Linguagem de estilo utilizada para estilizar elementos HTML', 'https://th.bing.com/th/id/R.7c12764796349bfb505d4c5d73489c3d?rik=bM4AFS0UAqY17g&pid=ImgRaw&r=0'),
-    (6, 'SQL', 'Linguagem de consulta estruturada para manipular bancos de dados', 'https://th.bing.com/th/id/OIP.7P45uhOM8sYH2dZVY_9XpQHaFk?rs=1&pid=ImgDetMain');
+    (6, 'SQL', 'Linguagem de consulta estruturada para manipular bancos de dados', 'https://th.bing.com/th/id/OIP.7P45uhOM8sYH2dZVY_9XpQHaFk?rs=1&pid=ImgDetMain'),
+	(7, 'React', 'Biblioteca JavaScript para criar interfaces de usuário', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png'),
+	(8, 'Node.js', 'Ambiente de execução JavaScript do lado do servidor',  'https://th.bing.com/th/id/OIP.Vegzcdv1Cl8Cex_jGgVK0wAAAA?rs=1&pid=ImgDetMain'),
+	(9, 'Django', 'Framework para desenvolvimento rápido de aplicações web em Python',  'https://www.svgrepo.com/show/353657/django-icon.svg'),
+	(10, 'Angular', 'Plataforma de desenvolvimento de aplicações web em TypeScript/JavaScript',  'https://seeklogo.com/images/A/angular-icon-logo-9946B9795D-seeklogo.com.png');
