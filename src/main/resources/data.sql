3d8be871d97d6ea7e6e46097d002482b246a2139
Júlia Lima <juliafclima@hotmail.com>
2024-07-19 14:37:48 -0300

    feat: imagens no banco

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 
 -- Inserir na tabela skill
 INSERT INTO public.Tab_Skill(nome_skill, descricao_skill, url_skill) VALUES 
-    ('Python', 'Linguagem de programação interpretada', 'https://th.bing.com/th/id/OIP.dJToM1TiZiJA0GYwzDHwjQHaHY?rs=1&pid=ImgDetMain'),
-    ('JavaScript', 'Linguagem de programação de alto nível', 'https://th.bing.com/th/id/R.f6302414f1a97b960efbc44cd0b7bf5e?rik=TQ%2bfMsswbcgl3w&pid=ImgRaw&r=0'),
-    ('Java', 'Linguagem de programação de propósito geral', 'https://th.bing.com/th/id/OIP.n8pa_ux7uUyU9CJrzb1scAHaHa?rs=1&pid=ImgDetMain'),
-    ('HTML', 'Linguagem de marcação utilizada na criação de páginas web', 'https://th.bing.com/th/id/OIP.lxMbLFRw0sgOlOwjC62ySwHaKc?rs=1&pid=ImgDetMain'),
-    ('CSS', 'Linguagem de estilo utilizada para estilizar elementos HTML', 'https://th.bing.com/th/id/R.7c12764796349bfb505d4c5d73489c3d?rik=bM4AFS0UAqY17g&pid=ImgRaw&r=0'),
-    ('SQL', 'Linguagem de consulta estruturada para manipular bancos de dados', 'https://th.bing.com/th/id/OIP.7P45uhOM8sYH2dZVY_9XpQHaFk?rs=1&pid=ImgDetMain'),
+    ('Python', 'Linguagem de programação interpretada', 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/Python.svg/640px-Python.svg.png'),
+    ('JavaScript', 'Linguagem de programação de alto nível', 'https://cdn3d.iconscout.com/3d/free/thumb/free-javascript-9294848-7577991.png?f=webp'),
+    ('Java', 'Linguagem de programação de propósito geral', 'https://cdn-icons-png.flaticon.com/512/5968/5968282.png'),
+    ('HTML', 'Linguagem de marcação utilizada na criação de páginas web', 'https://static-00.iconduck.com/assets.00/html-5-icon-224x256-1b5ud2sy.png'),
+    ('CSS', 'Linguagem de estilo utilizada para estilizar elementos HTML', 'https://lh4.googleusercontent.com/proxy/B-1PQKUCbPfJqXQaFmm1Y0Verbi9yQ8cw2cta17IXFKeqPP6gJcPs0fu-ASH2q_REfsZWwTXT92Om0BbrQlwsx0hwwF5jneLUr-r1EQ'),
+    ('SQL', 'Linguagem de consulta estruturada para manipular bancos de dados', 'https://desenvolvimentoaberto.org/wp-content/uploads/2016/11/logoazuresql.png'),
 	('React', 'Biblioteca JavaScript para criar interfaces de usuário', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png'),
-	('Node.js', 'Ambiente de execução JavaScript do lado do servidor',  'https://th.bing.com/th/id/OIP.Vegzcdv1Cl8Cex_jGgVK0wAAAA?rs=1&pid=ImgDetMain'),
+	('Node.js', 'Ambiente de execução JavaScript do lado do servidor',  'https://miro.medium.com/v2/resize:fit:900/1*TY9uBBO9leUbRtlXmQBiug.png'),
 	('Django', 'Framework para desenvolvimento rápido de aplicações web em Python',  'https://www.svgrepo.com/show/353657/django-icon.svg'),
 	('Angular', 'Plataforma de desenvolvimento de aplicações web em TypeScript/JavaScript',  'https://seeklogo.com/images/A/angular-icon-logo-9946B9795D-seeklogo.com.png');
 	
@@ -18,15 +18,16 @@ VALUES
 	
 INSERT INTO public.Tab_Usuario_Skill(id, level_usuario_skill, id_skill, id_usuario) 
 VALUES 
-	(1, '10', 1, 1),
-	(2, '5', 2, 1),
-	(3, '7', 3, 1),
-	(4, '8', 4, 1),
-	(5, '9', 5, 1),
-	(6, '2', 6, 1),
-	(7, '5', 7, 1),
-	(8, '6', 8, 1),
-	(0, '3', 9, 1);
+	(11, '10', 1, 1),
+	(12, '5', 2, 1),
+	(13, '7', 3, 1),
+	(14, '8', 4, 1),
+	(15, '9', 5, 1),
+	(16, '2', 6, 1),
+	(17, '5', 7, 1),
+	(18, '6', 8, 1),
+	(19, '3', 9, 1),
+	(20, '7', 10, 1);
 	
 INSERT INTO public.tab_Anotacao(data_anotacao, usuario_id, descricao_anotacao) 
 VALUES 
